3fee8dddc0dad505c09614273396169ada070c95
adhtri001 <adheeshtrivedi@gmail.com>
2026-06-15 00:14:07 +0530

    Basic hello world and added pixel art favicon

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -17,6 +17,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
 
 import App from './App.tsx'
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
